030e5f4b54ff399dd3677de3dda8bf064e39f2be
neilydhan <59085685+neilydhan@users.noreply.github.com>
2023-10-11 04:32:40 +0800

    stylish navigation bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,25 @@
 import '../styles/globals.css'
 import Link from 'next/link'
+import Image from "next/image"
+import Head from 'next/head'
+import LogoImage from '../logo.png'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="border-b px-12 py-6">
-        <p className="text-xl">NFT Marketplace</p>
+      <Head>
+        <title>Reddio NFT Marketplace</title>
+        <meta property="og:title" content="Reddio NFT Marketplace" key="title" />
+      </Head>
+      <nav className="w-full bg-gray-800 shadow">
+        <div className="flex items-center p-4 border-b text-center gap-x-4">
+          <Image src={LogoImage} alt="Logo" width="90"
+          height="20" />
+          <p className={`text-white origin-left font-medium text-xl`}>NFT Marketplace</p>
+        </div>
+        
+      </nav>
+      <nav className="border-b px-12 py-2">
         <div className="flex mt-4">
           <Link href="/">
             <a className="mr-4 text-blue-500">
